132dfbed51311ae3b3ed2f4bdca9080d91a898b2
Nicolas <nicolas@oktana.io>
2015-10-02 12:29:07 -0300

    FileManager & Image classes

---
+++ Example/Pods/Target Support Files/KSDeferred/KSDeferred-umbrella.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+#import "KSCancellable.h"
+#import "KSDeferred.h"
+#import "KSGenericsCompat.h"
+#import "KSNetworkClient.h"
+#import "KSNullabilityCompat.h"
+#import "KSPromise.h"
+#import "KSURLConnectionClient.h"
+#import "KSURLSessionClient.h"
+
+FOUNDATION_EXPORT double KSDeferredVersionNumber;
+FOUNDATION_EXPORT const unsigned char KSDeferredVersionString[];
+
